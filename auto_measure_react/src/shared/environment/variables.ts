28077096915d3120324c823b03f4f243e7b4a99f
Victor <victoorsantos266@gmail.com>
2024-09-16 00:44:11 -0300

    Prod

--- auto_measure_react/src/shared/environment/variables.ts
+++ auto_measure_react/src/shared/environment/variables.ts
@@ -1,3 +1,3 @@
 // Please provide the API_URL on .env if you want to use the app outside the server
-export const AppUrl = process.env.API_URL || 'http://127.0.0.1:3000';
-export const ENV = process.env.APP_ENVIROMENT || 'development';
+export const AppUrl = process.env.REACT_APP_API_URL || 'http://127.0.0.1:3000';
+export const ENV = process.env.REACT_APP_ENVIROMENT || 'development';
